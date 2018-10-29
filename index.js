a018fea4efdbb8dbec77c99a15955db7e20da728
ruben <rubenusshd@gmail.com>
2018-10-29 20:28:38 +0100

    finished google oauth serverside flow

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+require("./models/user");
+require("./services/passport");
+
+mongoose.connect("mongodb://localhost/google-oauth-setup");
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
   })
 );
 
-app.get("/auth/google/callback", passport.authenticate("google"));
+app.use(passport.initialize());
 
-app.listen(4999);
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
+app.listen(5000);
